53d494ccb9becd10cd93e0ce8fd14c7f7fc49968
Chris Nolan <chris@leytech.co.uk>
2017-04-29 09:08:17 +0100

    Prepping for public release

--- src/app/code/community/Leytech/RestrictAccess/Helper/Data.php
+++ src/app/code/community/Leytech/RestrictAccess/Helper/Data.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @package    Leytech_RestrictAccess
+ * @author     Chris Nolan (chris@leytech.co.uk)
+ * @copyright  Copyright (c) 2017 Leytech
+ * @license    https://opensource.org/licenses/MIT  The MIT License  (MIT)
+ */
+
 class Leytech_RestrictAccess_Helper_Data extends Mage_Core_Helper_Abstract
 {
+
 }
--- src/app/code/community/Leytech/RestrictAccess/Model/Observer.php
+++ src/app/code/community/Leytech/RestrictAccess/Model/Observer.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @package    Leytech_RestrictAccess
+ * @author     Chris Nolan (chris@leytech.co.uk)
+ * @copyright  Copyright (c) 2017 Leytech
+ * @license    https://opensource.org/licenses/MIT  The MIT License  (MIT)
+ */
+
 class Leytech_RestrictAccess_Model_Observer
 {
     public function checkAccess()
