238a8a8815f1af256d29c69627c886a76edf693c
Chris Nolan <chris@leytech.co.uk>
2017-04-29 12:07:48 +0100

    Add a notification to the admin area if extension is enabled

---
+++ src/app/code/community/Leytech/RestrictAccess/Block/Adminhtml/Notification.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package    Leytech_RestrictAccess
+ * @author     Chris Nolan (chris@leytech.co.uk)
+ * @copyright  Copyright (c) 2017 Leytech
+ * @license    https://opensource.org/licenses/MIT  The MIT License  (MIT)
+ */
+
+class Leytech_RestrictAccess_Block_Adminhtml_Notification extends Mage_Adminhtml_Block_Template
+{
+    /**
+     * Check if restrict access notice should be displayed
+     *
+     * @return boolean
+     */
+    public function displayRestrictAccessNotice()
+    {
+        return (bool) Mage::getStoreConfig(Leytech_RestrictAccess_Helper_Data::XML_PATH_IS_ENABLED);
+    }
+
+    /**
+     * Get management url
+     *
+     * @return bool
+     */
+    public function getManageUrl()
+    {
+        return (bool) $this->getUrl('adminhtml/system_config/edit/section/leytech_restrictaccess');
+    }
+
+    /**
+     * ACL validation for adding the link to the notification message
+     *
+     * @return bool
+     */
+    protected function canConfigure()
+    {
+        if (Mage::getSingleton('admin/session')->isAllowed('system/config/leytech_restrictaccess')) {
+            return true;
+        }
+        return false;
+    }
+
+}
