feed8aa31dc8d9714ea8ea57d154441e450bbda1
Chris Nolan <chris@leytech.co.uk>
2017-04-29 10:47:14 +0100

    Refactored code and removed unreliable admin session detection

--- src/app/code/community/Leytech/RestrictAccess/Helper/Data.php
+++ src/app/code/community/Leytech/RestrictAccess/Helper/Data.php
@@ -8,5 +8,83 @@
 
 class Leytech_RestrictAccess_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    const XML_PATH_IS_ENABLED = 'leytech_restrictaccess/settings/enabled';
+    const XML_PATH_ALLOWED_IPS = 'leytech_restrictaccess/settings/allowed_ips';
+    const XML_PATH_ALLOW_EMPTY_IP = 'leytech_restrictaccess/settings/allow_empty_ip';
+    const XML_PATH_MESSAGE = 'leytech_restrictaccess/settings/message';
+    const XML_PATH_ENABLE_LOG = 'leytech_restrictaccess/settings/enable_log';
+    const XML_PATH_LOG_FILE = 'leytech_restrictaccess/settings/log_file';
+
+    public function isEnabled() {
+        return Mage::getStoreConfig(self::XML_PATH_IS_ENABLED);
+    }
+
+    public function getAllowedIps() {
+        return Mage::getStoreConfig(self::XML_PATH_ALLOWED_IPS);
+    }
+
+    public function getAllowEmptyIp() {
+        return Mage::getStoreConfig(self::XML_PATH_ALLOW_EMPTY_IP);
+    }
+
+    public function getMessage() {
+        return Mage::getStoreConfig(self::XML_PATH_MESSAGE);
+    }
+
+    public function getEnableLog() {
+        return Mage::getStoreConfig(self::XML_PATH_ENABLE_LOG);
+    }
+
+    public function getLogFile() {
+        return Mage::getStoreConfig(self::XML_PATH_LOG_FILE);
+    }
+
+    /**
+     * Return whether the current IP address is allowed access
+     */
+    public function isIpAllowed()
+    {
+        // Get the remote IP address
+        $ip = Mage::helper('core/http')->getRemoteAddr();
+
+        // Allow empty IPs if configured otherwise reject them
+        if (!$ip) {
+            if($this->getAllowEmptyIp()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        // Allow if IP is in list of allowed IPs
+        $allowed = preg_split("/\r\n|\n|\r/", $this->getAllowedIps());
+        if (in_array($ip, $allowed)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Log any denied access requests
+     */
+    public function logDeniedAccess() {
+        // Only log if logging is enabled
+        if(!$this->getEnableLog()) {
+            return;
+        }
+        // Log the access attempt
+        Mage::log(
+            sprintf(
+                'Leytech_RestrictAccess: Access denied to %s from address %s',
+                    Mage::helper('core/url')->getCurrentUrl(),
+                    $_SERVER["REMOTE_ADDR"]
+            ),
+            null,
+            $this->getLogFile()
+        );
+    }
+
+
 
 }
--- src/app/code/community/Leytech/RestrictAccess/Model/Observer.php
+++ src/app/code/community/Leytech/RestrictAccess/Model/Observer.php
@@ -10,23 +10,24 @@ class Leytech_RestrictAccess_Model_Observer
 {
     public function checkAccess()
     {
+        $helper = Mage::helper('leytech_restrictaccess');
+
         // Do nothing if module is not active
-        if (!$this->_moduleActive()) {
-            return false;
+        if (!$helper->isEnabled()) {
+            return $this;
         }
 
         // Do nothing if current IP address is allowed
-        if ($this->_isIpAllowed()) {
-            return false;
+        if ($helper->isIpAllowed()) {
+            return $this;
         }
 
-        $adminurl = (string)Mage::getConfig()->getNode('admin/routers/adminhtml/args/frontName');
+        $helper->logDeniedAccess();
+        header('HTTP/1.0 403 Forbidden');
+        echo $helper->getMessage();
+        die();
 
-        $urlstring = Mage::helper('core/url')->getCurrentUrl();
-        $url = Mage::getSingleton('core/url')->parseUrl($urlstring);
-
-        if (strstr($url->path, "/{$adminurl}"))   return $this; // this is the admin section
-
+        /*
         // get admin session
         Mage::getSingleton('core/session', array('name' => 'adminhtml'))->start();
 
@@ -37,67 +38,12 @@ class Leytech_RestrictAccess_Model_Observer
 
         if (!$admin_logged_in)
         {
-            $this->_logDeniedAccess();
+            $helper->logDeniedAccess();
             header('HTTP/1.0 403 Forbidden');
-            echo $this->_getConfig('settings', 'message');
+            echo $helper->getMessage();
             die();
         }
-    }
-
-    /**
-     * Return the config value for the passed path and key
-     */
-    private function _getConfig($path, $key)
-    {
-        $fullpath = 'leytech_restrictaccess/' . $path . '/' . $key;
-        return Mage::getStoreConfig($fullpath, Mage::app()->getStore());
-    }
-
-    /**
-     * Return whether the extension has been enabled in the system configuration
-     */
-    private function _moduleActive()
-    {
-        return (bool)$this->_getConfig('settings', 'enable_ext');
-    }
-
-    /**
-     * Return whether the current IP address is allowed access
-     */
-    private function _isIpAllowed()
-    {
-        // Get the remote IP address
-        if(isset($_SERVER['REMOTE_ADDR'])) {
-            $ip = $_SERVER['REMOTE_ADDR'];
-        } else {
-            $ip = '';
-        }
-
-        // Allow if IP is empty and configured to allow empty IPs
-        if ($this->_getConfig('settings', 'allow_empty_ip') && empty($ip)) {
-            return true;
-        }
-
-        // Allow if IP is in list of allowed IPs
-        $allowed = explode(',', $this->_getConfig('settings', 'allowed_ips'));
-        if (in_array($ip, $allowed)) {
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Log any denied access requests
-     */
-    private function _logDeniedAccess() {
-        if(!(bool)$this->_getConfig('settings', 'enable_log')) {
-            return;
-        }
-        Mage::log(
-            sprintf('Restrict Access: Access denied to %s from address %s', Mage::helper('core/url')->getCurrentUrl(), $_SERVER["REMOTE_ADDR"]),
-            null,
-            $this->_getConfig('settings', 'log_file')
-        );
+        */
     }
 
 }
